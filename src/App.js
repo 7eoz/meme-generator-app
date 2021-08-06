d87d0d75a0c86012f0463d0c41d23512516d0ad4
leoSandrini <thesandrini@gmail.com>
2021-08-05 23:09:44 -0300

    setting up boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import Header from "./components/Header"
+import MemeGenerator from "./components/MemeGenerator"
+
 function App() {
   return (
     <div>
+      <Header />
+      <MemeGenerator />
     </div>
   );
 }
---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class MemeGenerator extends React.Component {
+    constructor(){
+        super()
+        this.state = {}
+    }
+    
+    render(){
+        return(
+            <h1>MEME GENERATOR</h1>
+        )
+    }
+}
+
+export default MemeGenerator
